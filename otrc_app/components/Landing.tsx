8a8fb8d2d2666167125797e71f20a85c863ed52f
Sio-Code <siochang777@gmail.com>
2023-02-17 23:08:35 +0000

    landing

---
+++ otrc_app/components/Landing.tsx
@@ -0,0 +1,34 @@
+import Image from "next/legacy/image"
+import React from "react"
+import Button from "./Button"
+
+function Landing() {
+  return (
+    <section className="sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8">
+      <div className="space-y-8">
+        <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
+          <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
+            Off The Road Collectives
+          </span>
+          <span className="block text-5xl">Hot Trending</span>
+          <span className="block text-5xl">Eco Chic Fashion</span>
+        </h1>
+
+        <div className="space-x-8">
+          <Button title="Buy Now" />
+          <a className="link">Learn More</a>
+        </div>
+
+        <div className="space-x-8">
+          <iframe width="40%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2FLiza_Dj%2Fcoffee-beats-vol25%2F" frameBorder="0" ></iframe>
+        </div>
+      </div>
+
+      <div className="relative hidden h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[600px]">
+        <Image src="/image_png_prod.png" layout="fill" objectFit="contain" alt={'land_image'}/>
+      </div>
+    </section>
+  );
+}
+
+export default Landing;
--- otrc_app/pages/index.tsx
+++ otrc_app/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Landing from '../components/Landing'
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,15 @@ const Home: NextPage = () => {
       </Head>
       <Header/>
 
+      <main className="relative h-[200vh] bg-[#E7ECEE]">
+        <Landing/>
+      </main>
+
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+        <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+          New Trending
+        </h1>
+      </section>
 
     </div>
   )
