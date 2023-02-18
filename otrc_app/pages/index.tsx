f084715ab3c73c8ba4b38fdffbae06cd8ff875c6
Sio-Code <siochang777@gmail.com>
2023-02-18 11:35:59 +0000

    before_changes

--- otrc_app/pages/index.tsx
+++ otrc_app/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
         <Landing/>
       </main>
 
-      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#f3ecea]">
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#292929]">
         <div className="space-y-10 py-16">
           <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
             New Trending
